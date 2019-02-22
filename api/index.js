827fa62fa595935b795c33e1439b85edff850822
Minh <hlminh2000@gmail.com>
2019-02-21 21:42:40 -0500

    tries out some weird stuff

--- api/index.js
+++ api/index.js
@@ -1,27 +1,28 @@
-const express = require("express");
-const router = express.Router();
-const { ApolloServer, gql } = require("apollo-server-express");
+module.exports = (req, res, next) => {
+  const express = require("express");
+  const router = express.Router();
+  const { ApolloServer, gql } = require("apollo-server-express");
 
-// The GraphQL schema
-const typeDefs = gql`
-  type Query {
-    "A simple type for getting started!"
-    hello: String
-  }
-`;
+  // The GraphQL schema
+  const typeDefs = gql`
+    type Query {
+      "A simple type for getting started!"
+      hello: String
+    }
+  `;
 
-// A map of functions which return data for the schema.
-const resolvers = {
-  Query: {
-    hello: () => "THIS IS DATA FROM GRAPHQL!!!"
-  }
+  // A map of functions which return data for the schema.
+  const resolvers = {
+    Query: {
+      hello: () => "THIS IS DATA FROM GRAPHQL!!!"
+    }
+  };
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers
+  });
+
+  server.applyMiddleware({ app: router });
+  router(req, res, next);
 };
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers
-});
-
-server.applyMiddleware({ app: router });
-
-module.exports = router;
