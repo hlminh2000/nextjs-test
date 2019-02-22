c11577192730e3a534a889d3a3b11dfae22fd223
Minh <hlminh2000@gmail.com>
2019-02-21 22:38:04 -0500

    more test

--- api.js
+++ api.js
@@ -1,1 +1,27 @@
-require("./api");
+const express = require("express");
+const router = express.Router();
+const { ApolloServer, gql } = require("apollo-server-express");
+
+// The GraphQL schema
+const typeDefs = gql`
+  type Query {
+    "A simple type for getting started!"
+    hello: String
+  }
+`;
+
+// A map of functions which return data for the schema.
+const resolvers = {
+  Query: {
+    hello: () => "THIS IS DATA FROM GRAPHQL!!!"
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
